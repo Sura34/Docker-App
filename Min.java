206b30bec8c8691e91974ed8c3fbc76533c66057
Surafel Alamneh <suramiheret@gmail.com>
2018-03-11 16:30:51 +0100

    failed test case

--- Min.java
+++ Min.java
@@ -2,11 +2,11 @@ public class Min {
 
 	public static int find_min(int[] a) {
 		int x, i;
-		x = a[0];
-		for(i = 1; i < a.length; i++){
-			if(a[i] < x)
-				x = a[i];
-		}
+		x = a[3];
+		//for(i = 1; i < a.length; i++){
+		//	if(a[i] < x)
+		//		x = a[i];
+		//}
 		return x;
 
 	}
